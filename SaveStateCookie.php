8a1554a67d9348cdca1ccf9a32aa8ed027f09c91
Petr Gostin <U.238@rambler.ru>
2019-09-25 17:18:22 +0300

    multiple changes

--- SaveStateCookie.php
+++ SaveStateCookie.php
@@ -8,12 +8,14 @@ use Pandora3\Libs\Time\Date;
 use Pandora3\Widgets\FieldFile\FieldFile;
 
 /**
- * Trait SaveStateCookie
- * @package Pandora3\Widgets\Form
- *
  * @property string $method
  */
 trait SaveStateCookie {
+
+	/**
+	 * @var string|null $cookieUri
+	 */
+	public $cookieUri = null;
 
 	/**
 	 * @param RequestInterface $request
@@ -21,22 +23,39 @@ trait SaveStateCookie {
 	 */
 	protected function loadFromRequest(RequestInterface $request): array {
 		$values = $request->all($this->method);
+		$cookieName = 'formData_'.preg_replace('#.*\\\\#', '', static::class);
 		if ($values) {
 			$cookies = Application::getInstance()->cookies;
-			$name = preg_replace('#.*\\\\#', '', static::class);
-			foreach ($values as $field => $value) {
+			foreach (array_intersect_key($values, array_flip($this->getSaveFields())) as $field => $value) {
 				if ($field instanceof FieldFile) {
 					continue;
 				}
 				$cookies->set(new Cookie(
-					"formData.$name.$field", $value, [
-						'path' => $request->uri,
+					$cookieName.'['.$field.']', $value, [
+						'path' => $this->getCookieUri($request),
 						'expire' => Date::now()->addInterval('1 months')
 					]
 				));
 			}
+		} else {
+			$values = $request->getCookie($cookieName) ?? [];
+			if ($values) {
+				$values = array_intersect_key($values, array_flip($this->getSaveFields()));
+			}
 		}
 		return $values;
+	}
+
+	protected function getSaveFields(): array {
+		return array_keys($this->fields);
+	}
+
+	/**
+	 * @param RequestInterface $request
+	 * @return string
+	 */
+	protected function getCookieUri(RequestInterface $request): string {
+		return $this->cookieUri ?? $request->uri;
 	}
 	
 }
